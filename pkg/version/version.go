6c89d3d8961f6831aa26c58c8fde808fa6c0d2e0
cgil <lao.tseu.is.alive@gmail.com>
2025-08-18 09:29:49 +0200

    changed url

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,8 +3,8 @@ package version
 var (
 	APP        = "jsonSiteGo"
 	AppSnake   = "json-site-go"
-	VERSION    = "0.2.4"
-	REPOSITORY = "https://github.com/lao-tseu-is-alive/JsonSiteGo"
+	VERSION    = "0.2.5"
+	REPOSITORY = "https://github.com/lao-tseu-is-alive/json-site-go"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
 )
